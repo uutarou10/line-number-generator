517df6793c094f79d25d57476da88c5371a1c157
Kota Nonaka <kota.kaicho@gmail.com>
2018-04-18 17:05:56 +0900

    add easteregg

--- src/index.js
+++ src/index.js
@@ -4,4 +4,22 @@ import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+const colors = ['red', 'green', 'blue', 'yellow', 'orange'];
+let index = 0;
+console.clear();
+setInterval(() => {
+  if (index + 1 > colors.length - 1) {
+    index = 0;
+  } else {
+    index += 1;
+  }
+
+  // console.clear();
+  console.log(
+    '%cWordを使うな、LaTeXを使え。',
+    `font-size: 300%; color: ${colors[index]}; font-weight: bold;`
+  );
+}, 100)
+
 registerServiceWorker();
